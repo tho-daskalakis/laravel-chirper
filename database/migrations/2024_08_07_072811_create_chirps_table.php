90304266ae8bdae3adaa6618c67fdd0701786c1b
thomas-d <tho.daskalakis@gmail.com>
2024-08-07 15:04:23 +0300

    implement user chirps creation

--- database/migrations/2024_08_07_072811_create_chirps_table.php
+++ database/migrations/2024_08_07_072811_create_chirps_table.php
@@ -14,6 +14,8 @@ return new class extends Migration
         Schema::create('chirps', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->string('message');
         });
     }
 
